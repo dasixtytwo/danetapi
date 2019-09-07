361fbd37cb0de3783c4cbaffdddea9c369ba8a09
Davide Agosti <davide.a@davideagosti.info>
2019-09-07 22:13:13 +0100

    Create Async and Migration DB

--- DaNetApi/Contracts/V1/Requests/CreatePostRequest.cs
+++ DaNetApi/Contracts/V1/Requests/CreatePostRequest.cs
@@ -4,6 +4,12 @@ namespace DaNetApi.Contracts.V1.Requests
 {
     public class CreatePostRequest
     {
-        public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public string SubTitle { get; set; }
+
+        public string Slug { get; set; }
+
+        public string BodyText { get; set; }
     }
 }
--- DaNetApi/Contracts/V1/Requests/UpdatePostRequest.cs
+++ DaNetApi/Contracts/V1/Requests/UpdatePostRequest.cs
@@ -8,5 +8,11 @@ namespace DaNetApi.Contracts.V1.Requests
     public class UpdatePostRequest
     {
         public string Title { get; set;}
+
+        public string SubTitle { get; set; }
+
+        public string Slug { get; set; }
+
+        public string BodyText { get; set; }
     }
 }
