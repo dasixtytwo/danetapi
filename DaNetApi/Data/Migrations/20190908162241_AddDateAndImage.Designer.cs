decdcc954616489da3bd5183591520ce548b25fa
Davide Agosti <davide.a@davideagosti.info>
2019-09-08 17:36:25 +0100

    Update Post Model and Add Migration

--- DaNetApi/Data/Migrations/20190908162241_AddDateAndImage.Designer.cs
+++ DaNetApi/Data/Migrations/20190908162241_AddDateAndImage.Designer.cs
@@ -4,14 +4,16 @@ using DaNetApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DaNetApi.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190908162241_AddDateAndImage")]
+    partial class AddDateAndImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DaNetApi/Data/Migrations/20190908162241_AddDateAndImage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DaNetApi.Data.Migrations
+{
+    public partial class AddDateAndImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PostDate",
+                table: "Posts",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostImage",
+                table: "Posts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostDate",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "PostImage",
+                table: "Posts");
+        }
+    }
+}
